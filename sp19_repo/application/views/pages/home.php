9a3e03797d2f3033bc9b22148ea7aef29b5418ba
Jeneva Scherr <scherrjeneva@gmail.com>
2019-04-16 16:08:55 -0700

    Add files via upload

---
+++ sp19_repo/application/views/pages/home.php
@@ -0,0 +1,5 @@
+<?php
+//applications/views/pages/home.php
+?>
+<h1>Welcome to my home page!</h1>
+<p>This is Jeneva Scherr from Seattle Central College ITC 260 class in Spring 2019.</p>
---
+++ sp19_repo/views/pages/about.php
@@ -0,0 +1,5 @@
+<?php
+//applications/views/pages/about.php
+?>
+<h1> Welcome to my about page!</h1>
+<p> We will be putting information in here soon.</p>
