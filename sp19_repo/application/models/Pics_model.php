9138f668768ae1b7c9df53b1ee3735b2d40417c5
Jeneva Scherr <scherrjeneva@gmail.com>
2019-04-27 22:12:03 -0700

    Update Pics_model.php

--- sp19_repo/application/models/Pics_model.php
+++ sp19_repo/application/models/Pics_model.php
@@ -15,27 +15,17 @@ public function get_pics($tags = FALSE)
         //SHOULD BE PASSED IN VIA QUERYSTRING/CONTROLLER
         //$tags = 'mariners';
         $perPage = 25;
-        $url.= 'https://api.flickr.com/services/rest/?method=flickr.photos.search';
+        $url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search';
         $url.= '&api_key='.$api_key;
         $url.= '&tags='.$tags;
         $url.= '&per_page='.$perPage;
         $url.= '&format=json';
         $url.= '&nojsoncallback=1';
             
-        $reponse = json_decode(file_get_contents($url));
+        $response = json_decode(file_get_contents($url));
         
-        return = $response->photos->photo;
+        return $response->photos->photo;
     
-    
-       /* if ($slug === FALSE)
-        {
-                $query = $this->db->get('sp19_news');
-                return $query->result_array();
-        }
-
-        $query = $this->db->get_where('sp19_news', array('slug' => $slug));
-        return $query->row_array();
-        */
 }
 
 }
